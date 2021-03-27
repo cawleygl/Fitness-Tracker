2ee4e3bf7dd8aab7bc6271fdedf5c22858e825c9
cawleygl <grantlcawley@gmail.com>
2021-03-27 12:13:42 -0400

    Added seeds file with sample workout data

---
+++ seeds.js
@@ -0,0 +1,19 @@
+const lastWorkout = {
+    "totalDuration": 1,
+    "day": 1616859307290,
+    "exercises": [
+      {
+        "name": "Bench Press",
+        "type": "resistance",
+        "weight": 1,
+        "sets": 1,
+        "reps": 1,
+      },
+      {
+        "name": "Mile Run",
+        "type": "cardio",
+        "distance": 1
+      }
+
+    ]
+  };
