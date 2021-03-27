39bcc0ca2f708e2b4386600fc3be9a3b2e880e6e
cawleygl <grantlcawley@gmail.com>
2021-03-27 12:14:52 -0400

    Updated model to correct format for front end

--- models/workout.js
+++ models/workout.js
@@ -4,33 +4,35 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     id: mongoose.ObjectId,
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for workout"
-    },
-    type: {
-        type: String,
-    },
-    weight: {
+    totalDuration: {
         type: Number,
     },
-    sets: {
-        type: Number,
-    },
-    reps: {
-        type: Number,
-    },
-    duration: {
-        type: Number,
-    },
-    distance: {
-        type: Number,
-    },
-    date: {
+    day: {
         type: Date,
         default: Date.now
-    }
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for workout"
+        },
+        type: {
+            type: String,
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        },    
+    }]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
