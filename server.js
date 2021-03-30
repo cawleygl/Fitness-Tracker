3a39931b1f139c31101e7c4fc612ddc41fc6dc24
cawleygl <grantlcawley@gmail.com>
2021-03-29 21:54:04 -0400

    Updated static path and Mongo server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var path = require('path');
+var router = express.Router();
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,11 +10,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', router);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
 
 // routes
